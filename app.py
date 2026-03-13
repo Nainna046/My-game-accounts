33ae9a2cde9a6138dc89d8e0c02b729cd5b84474
Naninna <myfangfc@gmail.com>
2026-03-14 00:49:48 +0700

    fix render database path

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash
+import os
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.secret_key = "mysecretkey"
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+os.makedirs("instance", exist_ok=True)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
